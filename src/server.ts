76c7c8b44c1004b1c7d2b5b9912d1ce8e1f13376
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-07-06 16:49:31 +0530

    init: initialize a basic Express server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+const port = Number(process.env.PORT) || 3000;
 
+app.use(cors());
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Hello TypeScript");
+  res.send("Pinterest clone works :)");
 });
 
-const port = Number(process.env.PORT) || 3000;
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok", message: "Server is healthy" });
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
