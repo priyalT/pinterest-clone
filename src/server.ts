5c1fc39203c81d422f72734232061d25170a8dc2
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-07-06 16:26:12 +0530

    chore: create entry point

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
+import "dotenv/config";
 import express from "express";
-import { configDotenv } from "dotenv";
 
 const app = express();
-configDotenv();
+
 
 app.get("/", (req, res) => {
   res.send("Hello TypeScript");
 });
 
-const port = process.env.PORT
+const port = Number(process.env.PORT) || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
